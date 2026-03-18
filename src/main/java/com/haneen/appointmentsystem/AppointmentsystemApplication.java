cdc6ad50caa407c93ee1d6ef840c84d087d16c4a
Haneen Hazem <haneen1802.hazem@gmail.com>
2026-03-18 02:52:39 +0200

    initial git setup

---
+++ src/main/java/com/haneen/appointmentsystem/AppointmentsystemApplication.java
@@ -0,0 +1,13 @@
+package com.haneen.appointmentsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppointmentsystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppointmentsystemApplication.class, args);
+    }
+
+}
